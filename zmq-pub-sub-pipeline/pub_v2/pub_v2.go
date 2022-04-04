545712dc971e92f2c6931ca8ba99dfd338a0a9db
phobyjun <phobyjun@gmail.com>
2022-04-04 16:04:42 +0900

    ADD: zmq-pub-sub-pipeline publisher v2

---
+++ zmq-pub-sub-pipeline/pub_v2/pub_v2.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	zmq "github.com/pebbe/zmq4"
+)
+
+func main() {
+	ctx, err := zmq.NewContext()
+	checkError(err)
+	defer ctx.Term()
+
+	publisher, err := ctx.NewSocket(zmq.PUB)
+	checkError(err)
+	defer publisher.Close()
+	publisher.Bind("tcp://*:5557")
+
+	collector, err := ctx.NewSocket(zmq.PULL)
+	checkError(err)
+	defer collector.Close()
+	collector.Bind("tcp://*:5558")
+
+	for {
+		message, err := collector.Recv(0)
+		checkError(err)
+
+		fmt.Printf("server: publishing update => %s\n", message)
+
+		publisher.Send(message, 0)
+	}
+}
+
+func checkError(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
