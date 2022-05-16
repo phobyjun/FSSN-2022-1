4c2572223ba5991d013e910cce4736cf79dc39bc
phobyjun <phobyjun@gmail.com>
2022-05-16 12:26:43 +0900

    ADD: client side streaming server and client

---
+++ client-streaming/client/client.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	pb "FSSN-2022-1/client-streaming/clientstreaming"
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+var (
+	addr = flag.String("addr", "localhost:50051", "the address to connect to")
+)
+
+func makeMessage(message string) *pb.Message {
+	return &pb.Message{Message: message}
+}
+
+func generateMessage() []*pb.Message {
+	messages := []*pb.Message{
+		makeMessage("message #1"),
+		makeMessage("message #2"),
+		makeMessage("message #3"),
+		makeMessage("message #4"),
+		makeMessage("message #5"),
+	}
+
+	return messages
+}
+
+func sendMessage(stub pb.ClientStreamingClient) {
+	messages := generateMessage()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	responses, err := stub.GetServerResponse(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, message := range messages {
+		if err := responses.Send(message); err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("[client to server] %s\n", message.Message)
+	}
+
+	response, err := responses.CloseAndRecv()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("[server to client] %d\n", response.Value)
+}
+
+func main() {
+	flag.Parse()
+	channel, err := grpc.Dial(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer channel.Close()
+
+	stub := pb.NewClientStreamingClient(channel)
+	sendMessage(stub)
+}
---
+++ client-streaming/server/server.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	pb "FSSN-2022-1/client-streaming/clientstreaming"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+var (
+	port = flag.Int("port", 50051, "The server port")
+)
+
+type server struct {
+	pb.UnimplementedClientStreamingServer
+}
+
+func (s *server) GetServerResponse(stream pb.ClientStreaming_GetServerResponseServer) error {
+	fmt.Println("Server processing gRPC client-streaming.")
+	var count int32 = 0
+	for {
+		_, err := stream.Recv()
+		if err == io.EOF {
+			return stream.SendAndClose(&pb.Number{
+				Value: count,
+			})
+		}
+		if err != nil {
+			return err
+		}
+		count += 1
+	}
+
+}
+
+func main() {
+	flag.Parse()
+	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", *port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	grpcServer := grpc.NewServer()
+	pb.RegisterClientStreamingServer(grpcServer, &server{})
+	log.Printf("server listening at %v", lis.Addr())
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to server: %v", err)
+	}
+}
